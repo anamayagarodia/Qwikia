8ba2e17e2e28b19a7c2229c1dad7df364da1fa1e
Ian Bantoto <lcbantoto@gmail.com>
2017-09-24 19:31:48 -0400

    forgot constant, added

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const DEBUG = true;
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
